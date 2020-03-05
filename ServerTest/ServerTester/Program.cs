ce1b6177d5eced3e5a254c0c814e06d181744eff
Shean92 <38415702+Shean92@users.noreply.github.com>
2020-03-05 10:53:46 -0700

    Add files via upload
    added false autoredirect to my http server testing section. This was why I wasn't getting any 300 status.

--- ServerTest/ServerTester/Program.cs
+++ ServerTest/ServerTester/Program.cs
@@ -18,15 +18,16 @@ namespace ServerTester
             //Change!!!!!
             string fileToReadFull = @"\\igxur\igxsites\cms101\pb24\lehi3-scraper\src\New Scraper c#\Collect\Collected_Files_employee.csv";
 
-            Console.WriteLine("This will overwrite all files that are currently on this file, are you ready?");
+            Console.WriteLine("This will overwrite all lines in the file you have specified, are you ready?");
             Console.ReadLine();
             int length = 0;
             ArrayList urls = new ArrayList();
-            // open the file "Collected_Data.csv"
+            // open the file "Collected_Files.csv"
             IEnumerable<string> lines = File.ReadLines(fileToReadFull);
+            lines = lines.Skip(1).ToArray();
             foreach (string line in lines)
             {
-                string[] delimitedLine = line.Split(',');
+                string[] delimitedLine = line.Split('|');
                 string newLine = delimitedLine[0].Replace(@"\\lehi3\emp$\", @"http://emp.byui.edu/");
                 newLine = newLine.Replace(@"\", @"/");
                 urls.Add(newLine);
@@ -41,10 +42,9 @@ namespace ServerTester
         static void TestServerStatus(ArrayList urls, int length, IEnumerable<string> linesOld)
         {
             string newFullPath = @"\\igxur\igxsites\cms101\pb24\lehi3-scraper\src\New Scraper c#\ServerTest\ServerTester\Server_Status_Final_employee.csv";
-            int status = 0;
             using (StreamWriter sw = File.CreateText(newFullPath))
             {
-                sw.WriteLine("Url, Status, Directory, Type, Size, Created, Modified, SubDirectories, Owner");
+                sw.WriteLine("Url| Status| Directory| Type| Size| Created| Modified| SubDirectories| Owner");
             }
             string errors = "";
 
@@ -52,6 +52,9 @@ namespace ServerTester
             int i = 0;
             foreach (string line in linesOld)
             {
+
+                int status = 0;
+                string[] delimitedLine = line.Split('|');
                 var watch = System.Diagnostics.Stopwatch.StartNew();
                 if(i > length)
                 {
@@ -64,16 +67,14 @@ namespace ServerTester
                 //status change
                 try
                 {
-                    var request = HttpWebRequest.Create(urls[i].ToString());
+                    HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(urls[i].ToString());
+                    request.AllowAutoRedirect = false;
                     request.Method = "HEAD";
                     request.Proxy = null;
                     ServicePointManager.DefaultConnectionLimit = 4;
                     using (var response = request.GetResponse() as HttpWebResponse)
                     {
-                        if (response != null)
-                        {
                             status = (int)response.StatusCode;
-                        }
                         response.Close();
                     }
                 }
@@ -82,31 +83,29 @@ namespace ServerTester
                     try
                     {
                         HttpWebResponse errorResponse = we.Response as HttpWebResponse;
-                        if (errorResponse.StatusCode == HttpStatusCode.NotFound)
-                        {
-                            status = (int)errorResponse.StatusCode;
-                        }
+                        status = (int)errorResponse.StatusCode;
                         errorResponse.Close();
                     }
                     catch (NullReferenceException)
                     {
-                        errors += line + " null reference exception";
+                        HttpWebResponse errorResponse = we.Response as HttpWebResponse;
+                        status = (int)errorResponse.StatusCode;
+                        errorResponse.Close();
                     }
                 }
                 catch (UriFormatException)
                 {
-                    errors += line + " format exception";
+                    errors += delimitedLine[0] + "\nformat exception\n";
                 }
                 catch (NullReferenceException)
                 {
-                    errors += line + " null reference exception";
+                    errors += delimitedLine[0] + "\nNull reference exception\n";
                 }
 
                 Console.WriteLine("Status recieved: " + status);
-                string[] delimitedLine = line.Split(',');
                 using (StreamWriter sw = new StreamWriter(newFullPath, true))
                 {
-                    string newLine = String.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8}", delimitedLine[0], status, delimitedLine[1], delimitedLine[2], delimitedLine[3], delimitedLine[4], delimitedLine[5], delimitedLine[6], delimitedLine[7]);
+                    string newLine = String.Format("{0}| {1}| {2}| {3}| {4}| {5}| {6}| {7}| {8}", delimitedLine[0], status, delimitedLine[1], delimitedLine[2], delimitedLine[3], delimitedLine[4], delimitedLine[5], delimitedLine[6], delimitedLine[7]);
                     sw.WriteLine(newLine);
                 }
                 i++;
